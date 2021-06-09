db3611e806f2b5ba1438b430d848e1d4726f793d
Dani Bellés Vallet <78848319+bellesdani@users.noreply.github.com>
2021-06-09 12:01:43 +0200

    Update Criba.java

--- ActividadEntornos/src/actividad/Criba.java
+++ ActividadEntornos/src/actividad/Criba.java
@@ -1,67 +1,135 @@
 package actividad;
 
-import java.util.*;
+import java.util.Scanner;
+
+/**
+ * 
+ * La clase Criba sirve para indicar los nÂº primos del 1 hasta el numero indicado por el usuario. 
+ * @author Daniel BellÃ©s
+ * @version 1.0.0
+ */
 
 public class Criba {
-	 
-// Generar números primos de 1 a max
-	public static int[] generarPrimos(int max) {
+	
+	/**
+	 * Metodo necesario para hacer los metodos de generacion de Primos
+	 * @param max
+	 * @return dim
+	 */
+	
+	public static int dim(int max) {
+		
 		int i, j;
 		if (max >= 2) {
-// Declaraciones
-			int dim = max + 1; // Tamaño del array
+			int dim = max + 1; 
 			boolean[] esPrimo = new boolean[dim];
-// Inicializar el array
+			
 			for (i = 0; i < dim; i++)
 				esPrimo[i] = true;
-// Eliminar el 0 y el 1, que no son primos
 			esPrimo[0] = esPrimo[1] = false;
 			
 			for (i = 2; i < Math.sqrt(dim) + 1; i++) {
 				if (esPrimo[i]) {
-// Eliminar los múltiplos de i
 					for (j = 2 * i; j < dim; j += i)
 						esPrimo[j] = false;
 				}
 			}
-// ¿Cuántos primos hay?
+			return dim;
+		}
+		else { 
+			return 0;
+		}
+	}
+	
+	/**
+	 * Metodo que devuelve un array de booleanos , en el que si es true, es primo
+	 * @param max
+	 * @return 
+	 */
+	
+public static boolean[] esPrimo(int max) {
+		
+		int i, j;
+		if (max >= 2) {
+			int dim = max + 1; 
+			boolean[] esPrimo = new boolean[dim];
+			
+			for (i = 0; i < dim; i++)
+				esPrimo[i] = true;
+			esPrimo[0] = esPrimo[1] = false;
+			
+			for (i = 2; i < Math.sqrt(dim) + 1; i++) {
+				if (esPrimo[i]) {
+					for (j = 2 * i; j < dim; j += i)
+						esPrimo[j] = false;
+				}
+			}
+			return esPrimo;
+		}else { 
+			return new boolean[0];
+		}
+	}
+	
+/**
+ * Array de enteros con todos los primos desde el 2 hasta el nÂº indicado por el usuario
+ * @param max
+ * @return primos
+ */
+
+	public static int[] generarPrimos(int max) {
+		
+		int i, j;
+		if (max >= 2) {
+		int dim=dim(max);
+		boolean[] esPrimo=esPrimo(max);
+			
 			int cuenta = 0;
 			for (i = 0; i < dim; i++) {
 				if (esPrimo[i])
 					cuenta++;
 			}
-// Rellenar el vector de números primos
 			int[] primos = new int[cuenta];
 			for (i = 0, j = 0; i < dim; i++) {
 				if (esPrimo[i])
 					primos[j++] = i;
 			}
 			return primos;
-		} else { // max < 2
+		} else { 
 			return new int[0];
-// Vector vacío
 		}
 	}
 
+	/**
+	 * Main de la clase , que pregunta un numero al usuario para ensenyarle los primos hasta Ã©l
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("Introduce el número para la criba de Erastótenes:");
+		System.out.println("Introduce el nÃºmero para la criba de ErastÃ³tenes:");
+		
 		int dato = teclado.nextInt();
 		int vector[] = new int[dato];
-		System.out.println("\nVector inicial hasta :" + dato);
+		teclado.close();
+		
+		System.out.println("\nVector inicial hasta : " + dato);
+		
 		for (int i = 0; i < vector.length; i++) {
 			if (i % 10 == 0)
 				System.out.println();
 			System.out.print(i + 1 + "\t");
 		}
+		
 		vector = generarPrimos(dato);
+		
 		System.out.println("\nVector de primos hasta:" + dato);
+		
 		for (int i = 0; i < vector.length; i++) {
 			if (i % 10 == 0)
 				System.out.println();
 			System.out.print(vector[i] + "\t");
-			System.out.println("PRIMER COMMIT");
 		}
+		System.out.println("REFACTORIZAMOS EL CÃ“DIGO");
 		teclado.close();
 	}
 }
